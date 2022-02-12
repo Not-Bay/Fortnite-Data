e0f1b30fd09a42b280f0f4be9a6fbb8c6ba9c617
Bay <60859158+BayGamerYT@users.noreply.github.com>
2022-02-11 20:34:54 -0600

    Add admin commands
    - Enable/disable commands command
    - Restart bot command
    - Reload cog command
    - Unload cog command
    - Load cog command

--- cogs/admin.py
+++ cogs/admin.py
@@ -26,6 +26,69 @@ class Admin(commands.Cog):
 
     @commands.is_owner()
     @commands.command(hidden=True)
+    async def _command(self, ctx, commandName: str):
+        """Enable/disable a command"""
+
+        try:
+            cmd = self.bot.get_command(commandName)
+
+            if cmd.enabled == True:
+                cmd.update(enabled = False)
+                await ctx.send(f'The command `{commandName}` was disabled.')
+            else:
+                cmd.update(enabled = True)
+                await ctx.send(f'The command `{commandName}` was enabled.')
+        except Exception as e:
+            await ctx.send(f'Could not enable/disable `{commandName}`: {e}')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def _restart(self, ctx):
+        """Stops the cogs/tasks running tasks and restarts the bot"""
+
+        try:
+            cog = self.bot.cogs['Tasks']
+
+            cog.shop_check.stop()
+            cog.updates_check.stop()
+
+            await ctx.send('The bot should restart in a few seconds.')
+            os.system('systemctl restart FortniteData.service')
+        except Exception as e:
+            await ctx.send(f'Could not restart: {e}')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def _reload(self, ctx, cog: str):
+        """Reloads a cog"""
+        try:
+            self.bot.reload_extension(f'cogs.{cog}')
+            await ctx.send(f'The cog `{cog}` was reloaded correctly.')
+        except Exception as e:
+            await ctx.send(f'Could not reload the cog: {e}')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def _load(self, ctx, cog: str):
+        """Load a cog"""
+        try:
+            self.bot.load_extension(f'cogs.{cog}')
+            await ctx.send(f'The cog `{cog}` was loaded correctly.')
+        except Exception as e:
+            await ctx.send(f'Could not load the cog: {e}')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def _unload(self, ctx, cog: str):
+        """Unload a cog"""
+        try:
+            self.bot.unload_extension(f'cogs.{cog}')
+            await ctx.send(f'The cog `{cog}` was unloaded correctly.')
+        except Exception as e:
+            await ctx.send(f'Could not unload the cog: {e}')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
     async def eval(self, ctx, *, cmd):
         """Evaluates code"""
         
