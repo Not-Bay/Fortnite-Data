d8edb70800222156cbf84f89193aff3999908b78
Bay <60859158+BayGamerYT@users.noreply.github.com>
2022-04-11 10:02:16 -0600

    Finish shop/updates config views

--- cogs/settings.py
+++ cogs/settings.py
@@ -2,7 +2,7 @@ from discord.commands import Option, OptionChoice, SlashCommandGroup, CommandPer
 from discord.ext import commands
 import discord
 
-from modules import util
+from modules import util, views
 
 class Settings(commands.Cog):
 
@@ -53,9 +53,9 @@ class Settings(commands.Cog):
 
         else:
 
-            server_data = util.database_get_server(ctx)
+            server = util.database_get_server(ctx)
 
-            if server_data['language'] == new_language:
+            if server['language'] == new_language:
 
                 await ctx.respond(
                     embed = discord.Embed(
@@ -69,7 +69,7 @@ class Settings(commands.Cog):
 
                 await ctx.respond(
                     embed = discord.Embed(
-                        description = util.get_str(lang, 'command_string_current_language_and_options').format(language = server_data['language'], options = 'en / es / ja'),
+                        description = util.get_str(lang, 'command_string_current_language_and_options').format(language = server['language'], options = 'en / es / ja'),
                         color = util.Colors.BLURPLE
                     )
                 )
@@ -91,6 +91,96 @@ class Settings(commands.Cog):
                         color = util.Colors.GREEN
                     )
                 )
+
+    @settings.command(
+        name='shop', 
+        description=util.get_str('en', 'command_description_settings_shop_channel'),
+        description_localizations={
+            'es-ES': util.get_str('es', 'command_description_settings_shop_channel'),
+            'ja': util.get_str('ja', 'command_description_settings_shop_channel')
+        }
+    )
+    @commands.cooldown(2, 4)
+    async def settings_shop_channel(
+        self,
+        ctx: discord.ApplicationContext
+    ):
+        
+        lang = util.get_guild_lang(ctx)
+
+        server = util.database_get_server(ctx)
+
+        if server['shop_channel']['enabled'] == False:
+            channel = util.get_str(lang, 'command_string_not_configurated')
+        else:
+            channel = f'<#{server["shop_channel"]["channel"]}>'
+
+        options_string = '\n'
+        for option in ['header', 'subheader', 'footer']:
+            options_string += f'`{option}` - {util.get_str(lang, "command_string_not_configurated") if server["shop_channel"]["config"][option] == None else server["shop_channel"]["config"][option]}\n'
+
+        view = discord.ui.View(
+            views.ShopChannelConfigure(lang),
+            views.ShopChannelManage(lang),
+            timeout = 120
+        )
+
+        await ctx.respond(
+            embed = discord.Embed(
+                title = util.get_str(lang, 'interaction_string_shop_config'),
+                description = util.get_str(lang, 'command_string_current_channel_and_options').format(
+                    channel = channel,
+                    options = options_string
+                ),
+                color = util.Colors.BLURPLE
+            ),
+            view = view
+        )
+
+    @settings.command(
+        name='updates', 
+        description=util.get_str('en', 'command_description_settings_updates_channel'),
+        description_localizations={
+            'es-ES': util.get_str('es', 'command_description_settings_updates_channel'),
+            'ja': util.get_str('ja', 'command_description_settings_updates_channel')
+        }
+    )
+    @commands.cooldown(2, 4)
+    async def settings_updates_channel(
+        self,
+        ctx: discord.ApplicationContext
+    ):
+        
+        lang = util.get_guild_lang(ctx)
+
+        server = util.database_get_server(ctx)
+
+        if server['updates_channel']['enabled'] == False:
+            channel = util.get_str(lang, 'command_string_not_configurated')
+        else:
+            channel = f'<#{server["updates_channel"]["channel"]}>'
+
+        options_string = '\n'
+        for option in ['shopsections', 'cosmetics', 'playlists', 'news', 'aes']:
+            options_string += f'`{util.get_str(lang, f"interaction_string_updates_option_{option}")}` - {util.get_str(lang, "command_string_disabled") if server["updates_channel"]["config"][option] == False else util.get_str(lang, "command_string_configured")}\n'
+
+        view = discord.ui.View(
+            views.UpdatesChannelConfigure(lang),
+            views.UpdatesChannelManage(lang),
+            timeout = 120
+        )
+
+        await ctx.respond(
+            embed = discord.Embed(
+                title = util.get_str(lang, 'interaction_string_updates_config'),
+                description = util.get_str(lang, 'command_string_current_channel_and_options').format(
+                    channel = channel,
+                    options = options_string
+                ),
+                color = util.Colors.BLURPLE
+            ),
+            view = view
+        )
 
 def setup(bot):
     bot.add_cog(Settings(bot))
