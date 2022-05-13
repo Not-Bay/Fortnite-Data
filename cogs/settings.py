e4dc4751f3cfb23e31cb8d9bfef400c261ba38f0
Bay <60859158+BayGamerYT@users.noreply.github.com>
2022-05-13 12:55:42 -0600

    Re-add `/settings language` command

--- cogs/settings.py
+++ cogs/settings.py
@@ -23,6 +23,87 @@ class Settings(commands.Cog):
     )
 
     @settings.command(
+        name=util.get_str('en', 'command_name_settings_language'),
+        name_localizations={
+            'es-ES': util.get_str('es', 'command_name_settings_language'),
+            'ja': util.get_str('ja', 'command_name_settings_language')
+        },
+        description=util.get_str('en', 'command_description_settings_language'),
+        description_localizations={
+            'es-ES': util.get_str('es', 'command_description_settings_language'),
+            'ja': util.get_str('ja', 'command_description_settings_language')
+        },
+    )
+    @commands.cooldown(2, 4)
+    async def settings_language(
+        self,
+        ctx: discord.ApplicationContext,
+        new_language: Option(
+            str,
+            description = 'New language to use',
+            default = 'none',
+            choices = [
+                OptionChoice(name = 'English', value = 'en'),
+                OptionChoice(name = 'Español', value = 'es'),
+                OptionChoice(name = '日本語', value = 'ja')
+            ]
+        )
+    ):
+
+        lang = util.get_guild_lang(ctx)
+
+        if ctx.author.guild_permissions.administrator == False:
+
+            await ctx.respond(
+                embed = discord.Embed(
+                    description = util.get_str(lang, 'command_string_only_admin_command'),
+                    color = util.Colors.RED
+                ),
+                ephemeral = True
+            )
+
+        else:
+
+            server = util.database_get_server(ctx)
+
+            if server['language'] == new_language:
+
+                await ctx.respond(
+                    embed = discord.Embed(
+                        description = util.get_str(lang, 'command_string_new_lang_same_as_old'),
+                        color = util.Colors.RED
+                    ),
+                    ephemeral = True
+                )
+
+            elif new_language == 'none':
+
+                await ctx.respond(
+                    embed = discord.Embed(
+                        description = util.get_str(lang, 'command_string_current_language_and_options').format(language = server['language'], options = 'en / es / ja'),
+                        color = util.Colors.BLURPLE
+                    )
+                )
+
+            else:
+
+                changes = util.database_update_server(
+                    ctx = ctx,
+                    changes = {
+                        '$set': {
+                            'language': new_language
+                        }
+                    }
+                )
+
+                await ctx.respond(
+                    embed = discord.Embed(
+                        description = util.get_str(lang, 'interaction_string_language_changed_to').format(lang = new_language),
+                        color = util.Colors.GREEN
+                    )
+                )
+
+    @settings.command(
         name=util.get_str('en', 'command_name_settings_shop_channel'),
         name_localizations={
             'es-ES': util.get_str('es', 'command_name_settings_shop_channel'),
