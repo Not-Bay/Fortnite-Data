2273c967a14edb6ac1341fa3881a1cd27634890f
Bay <60859158+BayGamerYT@users.noreply.github.com>
2022-09-08 18:18:46 -0600

    Create `language` module

---
+++ modules/language.py
@@ -0,0 +1,36 @@
+import aiofiles
+import logging
+import orjson
+
+log = logging.getLogger('FortniteData.modules.language')
+
+class Languages:
+
+    def __init__(self, languages: list):
+        self.languages = languages
+        self.data = dict()
+
+    async def initialize(self):
+
+        log.info('Initializing languages...')
+
+        for lang in self.languages:
+            try:
+                async with aiofiles.open(f'langs/{lang}.json', 'r', encoding='utf-8') as f:
+                    self.data[lang] = orjson.loads(await f.read())
+                    log.info(f'Loaded "{lang}".')
+            except:
+                log.error(f'An error ocurred loading language {lang}')
+
+    async def get_str(self, key: str, lang: str = 'en'):
+
+        language = self.data.get(lang, None)
+
+        if language == None:
+            log.error(f'Unable to return string, language "{lang}" is not loaded.')
+            return key
+
+        string = language.get(key, None)
+
+        if string == None:
+            log.error(f'String for "{key}" not found.')
